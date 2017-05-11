cff1accf5123046668d2d89f69a6055a1865ca8a
Alex Khatilov <akhatilov@wsgc.com>
2017-05-11 15:50:25 -0700

    Initial Commit

--- src/main/java/core/Anagram.java
+++ src/main/java/core/Anagram.java
@@ -6,7 +6,7 @@ public class Anagram {
 
 	public static boolean validate(String s, String t) {
 		if (s.replaceAll("\\s", "").length() != t.replaceAll("\\s", "").length()) {return false;}
-		
+		System.out.println("Entering test");
 		int value = 0;
 		for (int i = 0; i < s.replaceAll("\\s", "").length(); i++) {
 			value += (s.replaceAll("\\s", "").toLowerCase().charAt(i));
