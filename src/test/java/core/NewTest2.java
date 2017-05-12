d66edaa3ed04e660de6d26ddb10a2b7565afab08
Alex Khatilov <alex@alex.cc>
2017-05-12 01:07:01 -0700

    commit

--- src/test/java/core/NewTest2.java
+++ src/test/java/core/NewTest2.java
@@ -7,6 +7,17 @@ import org.testng.annotations.Test;
 
 public class NewTest2 {
 	
+	public void foo() {
+		for (int i = 0; i < 10; i++) {			// only references 'i'
+			for (int k = 0; k < 20; i++) {		// references both 'i' and 'k'
+				System.out.println("Hello");
+			}
+		}
+	}
+	void bar() {
+		for (;true;);
+	}
+	
 	@SuppressWarnings("null")
 	public static boolean A(String name) {
 		boolean n = (Boolean) null;
