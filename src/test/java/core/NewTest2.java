616356f4813e44d7c769cca215274b31a312df38
Alex Khatilov <alex@alex.cc>
2017-05-12 00:36:01 -0700

    commit

--- src/test/java/core/NewTest2.java
+++ src/test/java/core/NewTest2.java
@@ -6,6 +6,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import org.testng.annotations.Test;
 
 public class NewTest2 {
+	
+	@SuppressWarnings("null")
+	public static boolean A(String name) {
+		boolean n = (Boolean) null;
+		return n;
+	}
+	
 	@Test
 	public void test() {
 		assertThat((2 + 2), equalTo(4));
