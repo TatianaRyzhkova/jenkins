eae86774aa52073e3724242226e42c63045fd4e0
Alex Khatilov <akhatilov@wsgc.com>
2017-05-12 11:59:11 -0700

    Initial Commit

--- src/main/java/core/Anagram2.java
+++ src/main/java/core/Anagram2.java
@@ -17,5 +17,16 @@ public class Anagram2 {
 		}
 		return value == 0;
 	}
+	
+	public void foo() {
+		for (int i = 0; i < 10; i++) {			// only references 'i'
+			for (int k = 0; k < 20; i++) {		// references both 'i' and 'k'
+				System.out.println("Hello");
+			}
+		}
+	}
+	void bar() {
+		for (;true;);
+	}
 
 }
