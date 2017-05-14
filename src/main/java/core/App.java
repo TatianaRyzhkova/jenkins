73216207b83c8e169be3a3755096017c2ce3dd69
YuriiChukhrai <chuhray.uriy@gmail.com>
2017-05-13 20:28:49 -0700

    Update App.java

--- src/main/java/core/App.java
+++ src/main/java/core/App.java
@@ -4,16 +4,16 @@ public class App {
 	public static void main(String[] args) {
 		
 		System.out.println("Debit Card and Bad Credit is anagram? - " + Anagram.validate("Debit Card", "Bad Credit"));
-		System.out.println("Schoolmaster amnd The classroom is anagram? - " + Anagram.validate("Schoolmaster", "The classroom"));
-		System.out.println("The Hurricanes and These churn air is anagram? - " + Anagram.validate("The Hurricanes", "These churn air"));
-		System.out.println("Astronomers and No more stars is anagram? - " + Anagram.validate("Astronomers", "No more stars"));
-		System.out.println("Охренеть and Не хрена себе is anagram? - " + Anagram.validate("Охренеть", "Не хрена себе"));
+		//System.out.println("Schoolmaster amnd The classroom is anagram? - " + Anagram.validate("Schoolmaster", "The classroom"));
+		//System.out.println("The Hurricanes and These churn air is anagram? - " + Anagram.validate("The Hurricanes", "These churn air"));
+		//System.out.println("Astronomers and No more stars is anagram? - " + Anagram.validate("Astronomers", "No more stars"));
+		//System.out.println("Охренеть and Не хрена себе is anagram? - " + Anagram.validate("Охренеть", "Не хрена себе"));
 	
-		System.out.println("Debit Card and Bad Credit is anagram? - " + Anagram.validate("Debit Card", "Bad Credit"));
-		System.out.println("Schoolmaster amnd The classroom is anagram? - " + Anagram.validate("Schoolmaster", "The classroom"));
-		System.out.println("The Hurricanes and These churn air is anagram? - " + Anagram.validate("The Hurricanes", "These churn air"));
-		System.out.println("Astronomers and No more stars is anagram? - " + Anagram.validate("Astronomers", "No more stars"));
-		System.out.println("Охренеть and Не хрена себе is anagram? - " + Anagram.validate("Охренеть", "Не хрена себе"));
+		//System.out.println("Debit Card and Bad Credit is anagram? - " + Anagram.validate("Debit Card", "Bad Credit"));
+		//System.out.println("Schoolmaster amnd The classroom is anagram? - " + Anagram.validate("Schoolmaster", "The classroom"));
+		//System.out.println("The Hurricanes and These churn air is anagram? - " + Anagram.validate("The Hurricanes", "These churn air"));
+		//System.out.println("Astronomers and No more stars is anagram? - " + Anagram.validate("Astronomers", "No more stars"));
+		//System.out.println("Охренеть and Не хрена себе is anagram? - " + Anagram.validate("Охренеть", "Не хрена себе"));
 	
 	}
 }
