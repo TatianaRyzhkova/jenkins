5acb096ec2ad0b5fa41d0561576520f13af5dd28
Yurri Chukhari <ychukhari@wsgc.com>
2017-05-20 23:36:47 -0700

    PMD -4

--- src/main/java/core/FindBug.java
+++ src/main/java/core/FindBug.java
@@ -16,40 +16,65 @@ public class FindBug {
 	
 	// Violations 02 - Null pointer dereference
 	String test_02(List<String> list){
-	if ((list == null) && (list.size() == 0)) {     
-
-		  return null;
-
-		}
+//	if ((list == null) && (list.size() == 0)) {     
+//
+//		  return null;
+//
+//		}
 	return "" ;
 	}
 	
 	// Violations 03 - Dead store
 	void method1(){
-		int x=0;
-		while (true)	// not recommended
-		      x++;
-	}
+//		int x=0;
+//		while (true)	// not recommended
+//		      x++;
+//	}
 	
 	// Violations 04 - There is an apparent infinite loop
 	public void foo() {
-		for (int i = 0; i < 10; i++) {			// only references 'i'
-			for (int k = 0; k < 20; i++) {		// references both 'i' and 'k'
-				System.out.println("Hello");
-			}
-		}
+//		for (int i = 0; i < 10; i++) {			// only references 'i'
+//			for (int k = 0; k < 20; i++) {		// references both 'i' and 'k'
+//				System.out.println("Hello");
+//			}
+//		}
 		
 		// Violations 05 - There is an apparent infinite loop
 		Double newValue = null;
-		if ((newValue == Double.NaN) || (newValue < 0d)) {            
-
-			  // the code to execute
-
-			}
+//		if ((newValue == Double.NaN) || (newValue < 0d)) {            
+//
+//			  // the code to execute
+//
+//			}
 		
 		String[] array = "abc,fds".split(",");
 		System.out.println("Array "+array);
 	
 	}
+	
+
+public class Findbugs {
+
+	// FinalParametersCheck
+	@SuppressWarnings("null")
+	public boolean A(String name) {
+		boolean n = (Boolean) null;
+		return n;
+	}
+	
+	// getSecurePassword
+	 String getSecurePassword() {
+		
+		return "my-sec" + "ret-password";
+	}
+
+	 void abc() {	
+		int x = 2;
+		int y = 5;
+		double value1 =  x / y;
+
+	}
+
+}
 	
 }
